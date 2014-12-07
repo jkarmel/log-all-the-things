981b029f5a38561ca5387a76b3b3b51c1a4c4a84
Jeremy Karmel <jeremy@hired.com>
2014-12-07 12:10:08 -0800

    save request when it's created

--- lib/log_all_the_things.rb
+++ lib/log_all_the_things.rb
@@ -22,7 +22,7 @@ class ActionController::Base
 
   def log_request
     cookies[:cookied_browser_id] ||= CookiedBrowser.create!.id
-    request = Request.create cookied_browser_id: cookies[:cookied_browser_id]
+    request = Request.create! cookied_browser_id: cookies[:cookied_browser_id]
     Thread.current[:request] = request
     yield
   end
